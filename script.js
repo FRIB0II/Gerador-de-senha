b6a3aa563b847fb8cbd52ecf6fe120d855eece0c
FRIB0II <nicolasfigueiredo381@gmail.com>
2024-03-20 11:59:19 -0300

    Adicionado a escolha de special chars.

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let sliderElement = document.querySelector("#slider");
-let buttonElement = document.querySelector("#button");
+let buttonElement = document.querySelector("#button-generate");
+let buttonSpecialCharYes = document.querySelector("#button-special-char-yes");
+let buttonSpecialCharNo = document.querySelector("#button-special-char-no");
 
 let sizePassword = document.querySelector("#valor");
 let password = document.querySelector("#password");
 
 let containerPassword = document.querySelector("#container-password");
 
-let charset = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!@$&";
+let normalCharset = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+let specialCharset = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!@$&";
 let novaSenha = "";
 
 sizePassword.innerHTML = sliderElement.value;
@@ -18,13 +21,30 @@ slider.oninput = function(){
 function generatePassword(){
     
     let pass = "";
-    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
-        pass += charset.charAt(Math.floor(Math.random() * n))
+    for(let i = 0, n = normalCharset.length; i < sliderElement.value; ++i){
+        pass += normalCharset.charAt(Math.floor(Math.random() * n))
     }
     
-    containerPassword.classList.remove("hide");
+    containerPassword.classList.add("hide");
+
     password.innerHTML = pass;
     novaSenha = pass;
+}
+
+function generateSpecialPassword(){
+    let pass = "";
+    for(let i = 0, n = specialCharset.length; i < sliderElement.value; ++i){
+        pass += specialCharset.charAt(Math.floor(Math.random() * n))
+    }
+    
+    containerPassword.classList.add("hide");
+
+    password.innerHTML = pass;
+    novaSenha = pass;
+}
+
+function showPassword(){
+    containerPassword.classList.remove("hide");
 }
 
 function copyPassword(){
